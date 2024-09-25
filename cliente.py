bc4855dd063c902fad2f7a5b8751e79a051e0126
SofiaValadares <113111708+SofiaValadares@users.noreply.github.com>
2024-09-25 12:06:36 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -2,41 +2,38 @@ import socket
 import time
 
 # Função para enviar um pacote individual
-def enviar_pacote(mensagem, erro=False):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect(('localhost', 8081))
-
+def enviar_pacote(client_socket, mensagem, erro=False):
     # Simulação de erro de integridade
     if erro:
         mensagem = mensagem + "CORROMPIDO"  # Corrupção no pacote
 
-    client_socket.send(mensagem.encode())
-    response = client_socket.recv(1024)
-    print(f"Resposta do servidor: {response.decode()}")
-
-    client_socket.close()
+    client_socket.send(mensagem.encode()) # Enviar dados do cliente para o servidor
+    response = client_socket.recv(1024) # Receber uma resposta do servidor
+    print(f"Resposta do servidor: {response.decode()}") # Exibi resposta do servidor
 
 # Função para enviar vários pacotes em rajada
-def enviar_lote_pacotes(pacotes, erro=False):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect(('localhost', 8081))
+def enviar_lote_pacotes(client_socket, pacotes, erro=False):
     for pacote in pacotes:
         # Simula erro de integridade em pacotes específicos
         if erro:
             pacote = pacote + "CORROMPIDO"
 
-        client_socket.send(pacote.encode())
-        response = client_socket.recv(1024)
-        print(f"Resposta do servidor: {response.decode()}")
+        client_socket.send(pacote.encode()) # Enviar pacode do cliente para o servidor
+        response = client_socket.recv(1024) # Receber uma resposta do servidor
+        print(f"Resposta do servidor: {response.decode()}") # Exibi resposta do servidor
         time.sleep(1)  # Simula atraso no envio
 
-    client_socket.close()
 
 if __name__ == "__main__":
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Cria uma socket para o cliente
+    client_socket.connect(('localhost', 8081)) # Estabelece a conexão entre o cliente e o servidor
+
     # Teste com pacotes individuais
-    enviar_pacote("Pacote 1", erro=False)  # Pacote correto
-    enviar_pacote("Pacote 2", erro=True)   # Pacote com erro
+    enviar_pacote(client_socket, "Pacote 1", erro=False)  # Pacote correto
+    enviar_pacote(client_socket, "Pacote 2", erro=True)   # Pacote com erro
 
     # Teste com lotes de pacotes
     pacotes = ["Pacote 3", "Pacote 4", "Pacote 5"]
-    enviar_lote_pacotes(pacotes, erro=False)  # Envio normal de lotes
+    enviar_lote_pacotes(client_socket, pacotes, erro=False)  # Envio normal de lotes
+
+    client_socket.close() # Fecha a conexão do cliente com o seervidor
